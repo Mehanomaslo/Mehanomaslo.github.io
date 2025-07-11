109776b3c849ac9d3a44da05910de46bad25451d
Roman Fedyunin <85390860+Mehanomaslo@users.noreply.github.com>
2025-07-11 12:13:17 +0300

    Delete js directory

--- js/scroll.js
+++
@@ -1,8 +0,0 @@
-// ---------- scroll to block
-$('.scroll-to-block').click(function (e) {
-    e.preventDefault();
-    let target = $(this).data('block');
-    $('html, body').animate({
-        scrollTop: $(target).offset().top + 100 + "px"
-    }, 300);
-});
